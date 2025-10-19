53c1a1d0a8f2a8d317d9d4f21d9a103cd6ca7b25
Rahim10020 <rahialighi@gmail.com>
2025-10-19 12:34:05 +0000

    ajout du mouse effect dans page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import Experience from '@/components/sections/Experience';
 import Projects from '@/components/sections/Projects';
 import Footer from '@/components/sections/Footer';
 import Contact from '@/components/sections/Contact';
+import MouseEffect from '@/components/ui/MouseEffect';
 
 export default function Home() {
   return (
     <>
+      <MouseEffect />
       <div className="mx-auto min-h-screen max-w-6xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
         <div className="lg:flex lg:justify-between lg:gap-2">
           <Hero />
