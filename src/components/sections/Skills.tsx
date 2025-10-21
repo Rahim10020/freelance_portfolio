a9a2bf2a8bdf3ce7c760b076535aa90a26e57ed0
Rahim10020 <rahialighi@gmail.com>
2025-10-21 22:43:50 +0000

    resolution totale du souci de light mode et appliquage du light/dark au fichier skills.tsx

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -6,7 +6,7 @@ export default function Skills() {
             <ul className="flex flex-wrap gap-2">
                 {skills.map((skill) => (
                     <li key={skill}>
-                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                        <div className="flex items-center rounded-full bg-teal-500/20 dark:bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-700 dark:text-teal-300">
                             {skill}
                         </div>
                     </li>
