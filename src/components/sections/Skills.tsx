a3a2760fbbd4922c214ba485d2df61060255cca1
Rahim10020 <rahialighi@gmail.com>
2026-03-08 23:31:48 +0000

    fix skills problem

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,17 +1,20 @@
-import { skills } from '@/lib/data';
+import { skills } from "@/lib/data";
 
 export default function Skills() {
-    return (
-        <section id="skills" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
-            <ul className="flex flex-wrap gap-2">
-                {skills.map((skill) => (
-                    <li key={skill}>
-                        <div className="accent-chip flex items-center rounded-full px-3 py-1 text-xs font-medium leading-5">
-                            {skill}
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </section>
-    );
+  return (
+    <section
+      id="skills"
+      className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+    >
+      <ul className="flex flex-wrap gap-2">
+        {skills.map((skill) => (
+          <li key={skill}>
+            <div className="accent-chip flex items-center rounded-full px-3 py-1 text-xs font-medium font-display leading-5">
+              {skill}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
